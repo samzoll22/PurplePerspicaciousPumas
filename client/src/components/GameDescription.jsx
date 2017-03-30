145af33ed80c5459178a0d28911e3df7137ba020
samzoll22 <sam.zoll22@gmail.com>
2017-03-30 15:03:28 -0700

    Implemented Conditional Rendering of Login/Signup

--- client/src/components/GameDescription.jsx
+++ client/src/components/GameDescription.jsx
@@ -2,17 +2,40 @@
 import React from 'react';
 import { Col, Panel } from 'react-bootstrap';
 
-const GameDescription = (props) => (
-  <Col id="GameDescription" sm={6} smOffset={3}>
-    <h4>What Is It?</h4>
-    <Panel>
-      <b>Oranges to Oranges</b> is a four-player game where players try to come up 
-      with the funniest responses to thought-provoking questions. Sign up or 
-      log in below!
-    </Panel>
-    <br />
-  </Col>
-)
+
+class GameDescription extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      show: false
+    }
+    this.handleShowDescription = this.handleShowDescription.bind(this);
+  }
+
+  handleShowDescription () {
+    this.setState({
+      show: !this.state.show
+    })
+  }
+
+  render() {
+    return (
+      <Col id="GameDescription" sm={6} smOffset={3}>
+        <button type="button" className={"btn btn-default"} onClick={this.handleShowDescription}>What Is It?</button>
+
+        <div className={this.state.show ? 'show' : 'hide'}>
+          <Panel>
+            <b>Oranges to Oranges</b> is a four-player game where players try to come up
+            with the funniest responses to thought-provoking questions. Sign up or
+            log in below!
+          </Panel>
+        </div>
+        <br />
+      </Col>
+    )
+  }
+}
 
 
 export default GameDescription;
+
