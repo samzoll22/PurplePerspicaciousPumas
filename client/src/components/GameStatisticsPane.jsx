612a8636cff75727353ca1306380b3e1f2683b0b
kevinhuangg <email.kevinh@gmail.com>
2017-03-30 16:24:26 -0700

    added lines between each statistic

--- client/src/components/GameStatisticsPane.jsx
+++ client/src/components/GameStatisticsPane.jsx
@@ -3,14 +3,21 @@ import React from 'react';
 var GameStatisticsTab = (props) => {
   return (
     <div>
-      game stats tab active
+      <hr/>
       <div>username: {props.data.username}</div>
+      <hr/>
       <div>Games Won: {props.data.gamesWon}</div>
+      <hr/>
       <div>Games Lost: {props.data.gamesLost}</div>
+      <hr/>
       <div>Games Participated: {props.data.gamesParticipated}</div>
+      <hr/>
       <div>Currency: {props.data.inGameCurrency} oranges</div>
+      <hr/>
       <div>Current Win Streak: {props.data.currentWinStreak}</div>
+      <hr/>
       <div>Current Losing Streak: {props.data.currentLosingStreak}</div>
+      <hr/>
     </div>
   )
 }
